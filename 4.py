ce74578761d827107ecbcb1aeaccf1dff682eb65
Анна Железнякова <aleksandrabesse@gmail.com>
2020-12-03 22:43:17 +0300

    4_1_ready

--- 4.py
+++ 4.py
@@ -1,42 +1,42 @@
 # ctrl alt l
+import random
+import matplotlib.pyplot as plt
 import pandas
-from numba import njit, prange
+from numba import njit
+from datetime import datetime
+import numpy as np
 
 df = pandas.read_csv('titanic_data.csv')  # читаем данные из файла
-import numpy as np
 
 data = df[:len(df) - 1]  # удаляем последнюю строку, она нулевая
 count = data.shape[0]  # размер входных данных
 yData = np.array(data['Survived'].tolist(), dtype=np.int)  # массив меток, yi
 cols = [col for col in list(data) if col not in ['Name']]
 temp = [data[cols[k]].tolist() for k in range(1, len(cols))]
-xData = np.array([temp], dtype=np.float).T  # массив шестиразрядных векторов, xi
+xData = np.array([temp]).T  # массив шестиразрядных векторов, xi
 
 
-# @njit(fastmath=True)
-# def dot(a, b):
-#     mulp = 0;
-#     for i in range(len(a)):
-#         mulp += a[i] * b[i];
-#     return mulp;
-#
-#
-# o = np.ones((1, 5))
-# o1 = 2 * np.ones((5, 1))
-# print(dot(o[0], o1))
+@njit(fastmath=True)
+def dot(a, b):
+    mulp = 0
+    for i in range(a.shape[1]):
+        mulp += a[0][i] * b[i][0]
+    return mulp
+
+
+@njit(fastmath=True)
+def sigmoid(a):
+    return 1.0 / (1 + np.exp(-a))
 
 
 @njit(fastmath=True)
 def countFunctionGradient(x, y, z):
     z = z.T
+    temp = sigmoid(dot(z, x))
     if y == 0:
-        mulpWithPlus = np.dot(z, x)[0]
-
-        return x * mulpWithPlus / (1 + mulpWithPlus)
+        return -temp * x
     else:
-        mulpWithMinus = np.dot(-z, x)[0]
-
-        return x * np.exp(mulpWithMinus) / (1 + mulpWithMinus)
+        return (1 - temp) * x
 
 
 def gradientFunction(count, b):
@@ -46,80 +46,87 @@ def gradientFunction(count, b):
     return sum
 
 
-    # @ njit(fastmath=True)
-
-
+@njit(fastmath=True)
 def countLogicalRegressionFunction(x, y, z):
     z = z.T
-    if y==1:
-        mulpWithMinus = np.dot(-z, x)[0]
-        print('y==1')
-        return np.log2(1/(1+np.exp(mulpWithMinus)))
+    commonElement = sigmoid(dot(z, x))
+    if y == 0:
+        return np.log2(1 - commonElement)
     else:
-        mulpWithPlus = np.dot(z, x)[0]
-        print('y==0')
-        return np.log2(1/(1+np.exp(mulpWithPlus)))
+        return np.log2(commonElement)
 
 
-# @njit(fastmath=True)
 def logicalRegressionFunction(count, b):
     sum = 0
     for i in range(count):
         sum += countLogicalRegressionFunction(xData[i], yData[i], b)
     return sum
 
+def classify(x):
+    temp = sigmoid(dot(b.T, x))
+    if temp > 1 - temp:
+        return 1
+    else:
+        return 0
 
-from datetime import datetime
+# colvo = []
+# stepik = []
+# maximus = []
 
-print(datetime.now())
-from sympy import *
-
-# print(data.isnull().sum())  # проверка на пропущенные данныек
-x, y, z = symbols('x y z')
-function = log(((1 / (1 + exp(-z * x))) ** (y)) * ((1 / (1 + exp(z * x))) ** (1 - y)))
-gradient = diff(function, z)
-print(gradient)
-answers = []
-countIterations = 1000000
-
-import random
-
-curr = random.choice(xData)
-k = gradientFunction(int(0.8 * count), curr)
-print(k)
-b = curr+0.01*k
-print(b)
-log = logicalRegressionFunction(int(0.8 * count), b)
-print(log)
-def mainFunction():
-    global answers
-    currentVector = np.zeros((6, 1))
-    step = 0.000001
+def mainFunction(step, curr):
+    # global colvo, stepik, maximus
+    answers = []
+    countIterations = 100000
+    currentTime = datetime.now()
     for i in range(countIterations):
-        k = gradientFunction(int(0.8 * count), currentVector)
-        b = currentVector + step * k
-        answers.append((currentVector, logicalRegressionFunction(int(0.8 * count), b)))
-        currentVector = b
+        b = curr + step * gradientFunction(int(0.8 * count), curr)
+        o = logicalRegressionFunction(int(0.8 * count), b)
+        if o == -np.inf:
+            i -= 1
+        else:
+            answers.append((curr, o))
+        curr = b
+    # if len(answers) > 0:
+    maximum = answers[0]
+    ans = []
+    for i in answers:
+        ans.append(i[1])
+        if i[1] > maximum[1]:
+            maximum = i
+    print('Шаг', step, 'Количество значений', len(answers), 'Время выполнения',
+            str((datetime.now() - currentTime).total_seconds()), 'Максимальное значение функции ', maximum[1],
+            ' с вектором тэта  ', maximum[0])
+        # fig = plt.figure()
+        # ax = plt.axes()
+        # ax.scatter(range(0, len(ans)), ans)
+        # ax.set(xlabel='Итерации', ylabel='Значения функции l(q)',
+        #        title='Шаг=' + str(step))
+        # fig.savefig("test" + str(step) + ".png")
+        # stepik.append(step)
+        # colvo.append(len(answers))
+        # maximus.append(maximum[1])
 
 
-# mainFunction()
+# Подбор шага. Этап 1
+# steps = [0.0000001, 0.0000003, 0.0000005,0.0000006,0.0000007, 0.0000008, 0.0000009,0.000001, 0.000003, 0.000005, 0.000007, 0.000009,0.00001, 0.00003, 0.00005, 0.00007, 0.00009,0.0001, 0.0003, 0.0005, 0.0007, 0.0009, 0.001, 0.003, 0.005, 0.008, 0.009, 0.01, 0.03, 0.05, 0.07, 0.09, 0.1,
+#          0.3, 0.5,
+#          0.7, 0.9, 1]
 
-# maximum = answers[0]
-# ans = []
-# for i in answers:
-#     ans.append(i[1])
-#     if i[1] > maximum[1]:
-#         maximum = i
-# print(maximum)
-#
-# from mpl_toolkits import mplot3d
-# import numpy as np
-# import matplotlib.pyplot as plt
-#
+# steps = [0.0000001, 0.0000003, 0.0000005, 0.0000006, 0.0000007, 0.0000008, 0.0000009, 0.000001, 0.000003, 0.000005]
+# curr = random.choice(xData)
+# for step in steps:
+step = 5 * 10**(-6)
+mainFunction(step, np.zeros((6, 1)))
+
 # fig = plt.figure()
-#
-# # syntax for 3-D projection
 # ax = plt.axes()
-# print(datetime.now())
-# ax.scatter(range(0, countIterations), ans)
-# plt.show()
+# ax.plot(colvo, stepik)
+# ax.set(xlabel='Количество значений', ylabel='Шаг')
+# fig.savefig("test.png")
+# fig2 = plt.figure()
+# ax2 = plt.axes()
+# ax2.plot(stepik, maximus)
+# ax2.set(xlabel='Шаг', ylabel='Максимум')
+# fig2.savefig("max.png")
+
+
